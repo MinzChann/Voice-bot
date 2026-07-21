2a6caf6312da701b4da8dbe7f73f9896c30915f3
MinzChann <nnguyenhuynh797o@gmail.com>
2026-07-22 01:58:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
+const http = require('http');
 const { Client, GatewayIntentBits } = require('discord.js');
 const { joinVoiceChannel } = require('@discordjs/voice');
-const http = require('http');
 
-const TOKEN = process.env.DISCORD_TOKEN;
-const GUILD_ID = '1516850441256698030';
-const CHANNEL_ID = '1516858634506604695';
-
-http.createServer((req, res) => res.end('OK')).listen(process.env.PORT || 3000);
-
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
-});
-
-client.once('ready', () => {
-    console.log(`Bot online: ${client.user.tag}`);
-    const guild = client.guilds.cache.get(GUILD_ID);
-    const channel = guild?.channels.cache.get(CHANNEL_ID);
-    if (guild && channel) {
-        joinVoiceChannel({
-            channelId: channel.id,
-            guildId: guild.id,
-            adapterCreator: guild.voiceAdapterCreator,
-            selfDeaf: true,
-            selfMute: true
-        });
-        console.log('Da vao voice!');
-    }
-});
-
-client.login(TOKEN);
-const http = require('http');
 http.createServer((req, res) => res.end('Bot is running!')).listen(process.env.PORT || 3000);
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates
+  ]
+});
+
+client.on('clientReady', async () => {
+  console.log(`Bot online: ${client.user.tag}`);
+
+  try {
+    const channel = await client.channels.fetch('1516858634506604695');
+
+    if (channel) {
+      joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+        selfDeaf: true,
+        selfMute: false
+      });
+      console.log('Da vao voice thanh cong!');
+    }
+  } catch (error) {
+    console.error('Loi khi vao voice:', error);
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
